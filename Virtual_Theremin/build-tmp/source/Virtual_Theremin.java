57faa94febb7a4fc685c8d7964ddd16bccf7d9e2
amrishparmar <amrish.parmar@me.com>
2015-03-05 13:56:55 +0000

    Add basic camera functionality to theremin

--- Virtual_Theremin/build-tmp/source/Virtual_Theremin.java
+++ Virtual_Theremin/build-tmp/source/Virtual_Theremin.java
@@ -1,9 +1,27 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import processing.video.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Virtual_Theremin extends PApplet {
+
   
-import processing.video.*;
+
 
 Capture cam;
 
-void setup() {
+public void setup() {
   size(640, 480);
 
   String[] cameras = Capture.list();
@@ -23,7 +41,7 @@ void setup() {
   }      
 }
 
-void draw() {
+public void draw() {
   if (cam.available() == true) {
     cam.read();
   }
@@ -36,4 +54,13 @@ void draw() {
   fill(0);
   textSize(30);
   text("FPS: "+frameRate, 10, 30);
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Virtual_Theremin" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
